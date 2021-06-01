59d53df2c134486beef16fd6a5e2605004394951
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-01 16:29:44 -0400

    Update placeholder functions

--- LASSCoinChain/TransactionBlock.py
+++ LASSCoinChain/TransactionBlock.py
@@ -1,3 +1,12 @@
-class Block:
-    def __init__(self):
+import datetime
+from hashlib import sha256
+
+class TransactionBlock:
+    def __init__(self, height, previous_hash, sender_address, target_address, value, signature):
+        pass
+
+    def verify_signature(self):
+        pass
+
+    def hash(self):
         pass
