0d24d484330fa5d9dbfb60a0dfb2b39b0359779d
Krish120003 <krish120003@gmail.com>
2021-06-02 09:00:35 -0400

    Add GenesisBlocks, create_transaction, make blocks tuple array to store miner

--- src/Blockchain/Chain.py
+++ src/Blockchain/Chain.py
@@ -1,8 +1,22 @@
 from .TransactionBlock import TransactionBlock
 
+from hashlib import sha256
+import arrow
+
 
 def clamp(x, MAX, MIN):
     return min(MAX, max(x, MIN))
+
+
+class GenesisBlock:
+    def __init__(self):
+        self.height = 0
+        self.timestamp = arrow.now()
+
+    def hash(self):
+        h = sha256()
+        h.update(str(self.__dict__).encode("utf-8"))
+        return h.hexdigest()
 
 
 class BlockChain:
@@ -17,18 +31,21 @@ class BlockChain:
 
     def create(difficulty=1):
         chain = BlockChain(clamp(difficulty, 63, 1))
+        # Create genesis block
+        chain.blocks.append((GenesisBlock(), None))
         return chain
 
-    def add_block(self, block):
+    def add_block(self, block, miner=None):
         assert isinstance(block, TransactionBlock)
         assert block.height == len(self.blocks)
+
         self.blocks.sort(key=lambda block: block.height)
         for i in range(self.max_nonce):
             # Hash Block
             hash = block.hash()
             if hash.startswith(self.target):
                 # Nonce found
-                self.blocks.append(block)
+                self.blocks.append((block, miner))
                 return
 
             # Increase Nonce
@@ -36,12 +53,13 @@ class BlockChain:
 
         raise Exception("No valid block nonce found.")
 
-    def create_transaction(ctx):
-        # TO-DO
-        pass
-
-
-if __name__ == "__main__":
-    x = BlockChain.create(1)
-    print(x.__dict__)
-    print(TransactionBlock)
+    def create_transaction(self, ctx):
+        block = TransactionBlock(
+            len(self.blocks),
+            self.blocks[-1].hash(),
+            ctx.sender,
+            ctx.target,
+            ctx.amount,
+            ctx.signature,
+        )
+        return block
