c3ac35b3f1c9ca7b5325e6f8ce34d416896f1cd4
Krish120003 <krish120003@gmail.com>
2021-06-02 18:57:05 -0400

    Basic blockchain file loading

--- src/WalletUI/main.py
+++ src/WalletUI/main.py
@@ -5,9 +5,10 @@ from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtCore import QObject, QTimer, Slot, Signal
 
-
 import logging
 import coloredlogs
+
+from Blockchain import Chain, load_chain_from_file
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
@@ -15,16 +16,29 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-coloredlogs.install(level="INFO")
+coloredlogs.install(level="DEBUG")
 
 
 class QtAppInterface(QObject):
     def __init__(self):
         QObject.__init__(self)
 
+        self.chain = None
+        self.wallet_key = None
+
     @Slot(str)
     def loadChain(self, path):
-        print(path)
+        if path.startswith("file://"):
+            path = path[len("file://") :]
+        try:
+            self.chain = load_chain_from_file(path)
+            logging.info(f"Load {path} successful")
+        except Exception as e:
+            logging.error(f"Load {path} failed")
+            logging.error(e)
+            # show popup that it failed
+            return
+        logging.info(self.chain)
 
 
 if __name__ == "__main__":
