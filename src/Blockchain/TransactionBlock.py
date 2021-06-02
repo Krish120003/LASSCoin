63a92e068048c9a9704070d0c8ac6e0a102a1381
Krish120003 <krish120003@gmail.com>
2021-06-01 23:42:45 -0400

    Setup context in Block

--- src/Blockchain/TransactionBlock.py
+++ src/Blockchain/TransactionBlock.py
@@ -1,6 +1,6 @@
 import arrow
 from hashlib import sha256
-from . import Context
+from .Context import Context
 
 
 class TransactionBlock:
@@ -8,17 +8,27 @@ class TransactionBlock:
         self, height, previous_hash, sender_address, target_address, value, signature
     ):
         self.nonce = 0
-        self.timestamp = arrow.now().timestamp
+        self.timestamp = arrow.now()
 
         self.height = height
         self.previous_hash = previous_hash
 
         self.context = Context(sender_address, target_address, value, signature)
 
-        self.context.verify()
-
-    def create_context(self):
-        pass
+        # self.context.verify()
 
     def hash(self):
         pass
+
+    def as_dict(self):
+        # Transaction Info
+        context = self.context.__dict__
+        context.pop("_ctx")
+        # Block Info
+        data = {
+            "nonce": self.nonce,
+            "timestamp": int(self.timestamp.timestamp()),
+            "height": self.height,
+            "previous_hash": self.previous_hash,
+        } | context
+        return data
