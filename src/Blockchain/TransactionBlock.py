edfcc468e1da8898180a1501c71563aa38b80fa8
Krish120003 <krish120003@gmail.com>
2021-06-01 22:45:06 -0400

    Setup Init

--- src/Blockchain/TransactionBlock.py
+++ src/Blockchain/TransactionBlock.py
@@ -3,12 +3,21 @@ from hashlib import sha256
 
 
 class TransactionBlock:
-    def __init__(self, height, previous_hash, sender_address, target_address, value, signature):
+    def __init__(
+        self, height, previous_hash, sender_address, target_address, value, signature
+    ):
         self.nonce = 0
         self.timestamp = arrow.now().timestamp
 
         self.height = height
         self.previous_hash = previous_hash
+
+        self.sender_address = sender_address
+        self.target_address = target_address
+        self.transaction_value = value
+        self.signature = signature
+
+        self.verify_signature()
 
     def create_context(self):
         pass
