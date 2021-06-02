4e1f3a5be91612b21539d19ebceca164b4a4a7bb
Krish120003 <krish120003@gmail.com>
2021-06-01 23:06:21 -0400

    Update __init__.py

--- src/KeyManager/KeyManager/__init__.py
+++ src/KeyManager/KeyManager/__init__.py
@@ -7,24 +7,24 @@ import base64
 
 
 def generate(nbits):
-    '''
+    """
     Simple wrapper to generate an RSA key.
-    '''
+    """
     return RSA.generate(nbits)
 
 
 def save_key(key, path):
-    '''
+    """
     Saves the private key as a binary file.
-    '''
+    """
     with open(os.path.join(path, "private.key"), "wb") as f:
         f.write(key.export_key())
 
 
 def load_key(path):
-    '''
+    """
     Loads a private key from a binary file.
-    '''
+    """
     try:
         with open(path, "rb") as f:
             # Import File
@@ -36,37 +36,37 @@ def load_key(path):
 
 
 def encode(data):
-    '''
+    """
     Encode binary data into a string
     using base64 encoding.
-    '''
+    """
     return base64.b64encode(data)
 
 
 def decode(data):
-    '''
+    """
     Decode a string to binary data
     using base64 encoding.
-    '''
+    """
     return base64.b64decode(data)
 
 
 def sign(priv_key, message):
-    '''
+    """
     Sign a message with the private key.
-    '''
-    hash = SHA256.new(message.encode('utf-8'))
+    """
+    hash = SHA256.new(message.encode("utf-8"))
     signer = pkcs1_15.new(priv_key)
     signature = signer.sign(hash)
     return encode(signature)
 
 
 def verify(public_key, message, signature):
-    '''
+    """
     Verify a message's integrity using a signature.
-    '''
+    """
     verifier = pkcs1_15.new(public_key)
-    hash = SHA256.new(message.encode('utf-8'))
+    hash = SHA256.new(message.encode("utf-8"))
     try:
         verifier.verify(hash, decode(signature))
         return True
