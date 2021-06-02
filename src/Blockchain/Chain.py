007e2fcaaee6140052b0cdcf0ed63d6b20515c3d
Krish120003 <krish120003@gmail.com>
2021-06-02 00:52:34 -0400

    Create block adding function, placeholder create function

--- src/Blockchain/Chain.py
+++ src/Blockchain/Chain.py
@@ -1,3 +1,6 @@
+from .TransactionBlock import TransactionBlock
+
+
 def clamp(x, MAX, MIN):
     return min(MAX, max(x, MIN))
 
@@ -9,13 +12,36 @@ class BlockChain:
         # Constants
         self.max_nonce = 2 ** 32
         self.target = "0" * self.difficulty
+        self.blocks = []
         pass
 
     def create(difficulty=1):
-        chain = BlockChain(clamp(difficulty, 31, 1))
+        chain = BlockChain(clamp(difficulty, 63, 1))
         return chain
+
+    def add_block(self, block):
+        assert isinstance(block, TransactionBlock)
+        assert block.height == len(self.blocks)
+        self.blocks.sort(key=lambda block: block.height)
+        for i in range(self.max_nonce):
+            # Hash Block
+            hash = block.hash()
+            if hash.startswith(self.target):
+                # Nonce found
+                self.blocks.append(block)
+                return
+
+            # Increase Nonce
+            block.nonce += 1
+
+        raise Exception("No valid block nonce found.")
+
+    def create_transaction(ctx):
+        # TO-DO
+        pass
 
 
 if __name__ == "__main__":
     x = BlockChain.create(1)
     print(x.__dict__)
+    print(TransactionBlock)
