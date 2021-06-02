08da3ce08ba97c7f27f392ccf37e214ca80384dd
Krish120003 <krish120003@gmail.com>
2021-06-02 18:09:10 -0400

    setup logging in ui backend

--- src/WalletUI/main.py
+++ src/WalletUI/main.py
@@ -5,6 +5,16 @@ import os
 from PySide2.QtGui import QGuiApplication
 from PySide2.QtQml import QQmlApplicationEngine
 
+import logging
+import coloredlogs
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.DEBUG,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+coloredlogs.install(level="INFO")
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
