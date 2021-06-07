4dfabfb75e0cb872ac8ca480491f3ef504b87ff6
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-07 09:03:02 -0400

    Add basic transactions endpoint

--- src/Backend/app.py
+++ src/Backend/app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return "Hello LASSCoin"
 
+@app.route("/api/transactions/create", methods=["POST"])
+def create_transaction(**kwargs):
+    return kwargs
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
