6716fa531a6132c778e621e653926da0a8e8aa43
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-07 11:39:48 -0400

    setup project docs

--- src/API/app.py
+++ src/API/app.py
@@ -3,12 +3,12 @@ import uvicorn
 
 from .models import CreateTransactionContext
 
-app = FastAPI()
+app = FastAPI(title="LASSCoin Backend", version="a1.0", description="API Backend to process LASSCoin transactions.")
 
 @app.post("/api/transactions/")
 def create_transaction(x: CreateTransactionContext):
     print(x)
-    return {}
+    return dict(x)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=5000, reload=True)
