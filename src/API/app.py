45181178d4d49eb00aaf6e7e16c073a497dbbeaa
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-07 10:52:41 -0400

    setup first route, test pydantic

--- src/API/app.py
+++ src/API/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
@@ -6,6 +7,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+class Test(BaseModel):
+    name: str
+
+@app.post("/api/transactions/")
+def create_transaction(x: Test):
+    print(x)
+    return {}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=5000, reload=True)
