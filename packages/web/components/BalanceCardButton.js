023813587f8b1e8ed0480e6fa3c3246dc6aff691
Krish120003 <krish120003@gmail.com>
2021-06-14 18:32:14 -0400

    Basic setup buttons

---
+++ packages/web/components/BalanceCardButton.js
@@ -0,0 +1,11 @@
+import styles from "../styles/BalanceCardButton.module.scss";
+
+export default function BalanceCardButton(props) {
+  return (
+    <div
+      className={!props.red ? styles.button : `${styles.button} ${styles.red}`}
+    >
+      <p className={styles.button_text}>{props.value}</p>
+    </div>
+  );
+}
