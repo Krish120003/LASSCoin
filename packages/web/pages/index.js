6c2dd86b5eca66a74fb65669ad208df7bc8194ba
AidanP771 <aidanm771@outlook.com>
2021-06-14 16:35:42 -0400

    Changed Import and call to match name

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MyApp from './_app'
 import BalanceCard from '../components/BalanceCard'
-import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
         <Head> 
           <title> PAIN </title>
         </Head>
-        <Footer/>
+        <Navbar/>
       </>
   )
 };
