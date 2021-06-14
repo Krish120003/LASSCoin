10eeb41096c51320e5f90689ced48158403ca214
AidanP771 <aidanm771@outlook.com>
2021-06-14 14:07:03 -0400

    AAAAAAAAAAA!!!!!!!!!!!!

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import MyApp from './_app'
 
-export default function Home() {
-  return <p>Hello World!</p>
+function Home() {
+  return <div> I have no idea how to use this!!!! </div>
 }
+
+export default Home
