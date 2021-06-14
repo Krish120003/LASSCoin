314006be2aefac672073b0d117f4e349e96f3311
Krish120003 <krish120003@gmail.com>
2021-06-14 18:32:53 -0400

    Setup basic layout in Balance Card

--- packages/web/components/BalanceCard.js
+++ packages/web/components/BalanceCard.js
@@ -1,5 +1,16 @@
 import styles from "../styles/Balance.module.scss";
+import TextColumnGroup from "./TextColumnGroup";
+import BalanceCardButton from "./BalanceCardButton";
 
 export default function BalanceCard() {
-  return <div className={styles.balance_card}>e</div>;
+  return (
+    <div className={styles.balance_card}>
+      <h3 className={styles.thold_title}>Balance Details</h3>
+      <TextColumnGroup />
+      <div className={styles.buttons}>
+        <BalanceCardButton value={"Receive"} red={false} />
+        <BalanceCardButton value={"Send"} red={true} />
+      </div>
+    </div>
+  );
 }
