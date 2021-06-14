403999473773cf440ad86468055cd7b562e91388
AidanP771 <aidanm771@outlook.com>
2021-06-14 14:01:28 -0400

    imported app.js to index

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import MyApp from './_app'
 
 export default function Home() {
-  return <p>Hello</p>
+  return <p>Hello World!</p>
 }
