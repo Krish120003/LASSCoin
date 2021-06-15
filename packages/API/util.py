0a72d094de59fd794ff03c466747f031aa267c9a
Krish120003 <krish120003@gmail.com>
2021-06-15 19:14:21 -0400

    Add function to jsonize transactions

--- packages/API/util.py
+++ packages/API/util.py
@@ -3,3 +3,10 @@ from db import PendingTransaction  # , Transactions
 
 def get_max_height(db):
     return db.query(PendingTransaction).count()  # + db.query(Transactions)
+
+
+def serialize_transaction(data):
+    x = data.__dict__
+    x.pop("_sa_instance_state")
+    x["time"] = int(x["time"].timestamp())
+    return x
