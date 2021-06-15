a05492212edba17004ec2293dcbfbbe469a05265
Krish120003 <krish120003@gmail.com>
2021-06-15 18:35:52 -0400

    Create Transaction class

--- packages/API/db.py
+++ packages/API/db.py
@@ -14,6 +14,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 class PendingTransaction(Base):
     __tablename__ = "PendingTransactions"
 
@@ -26,6 +27,25 @@ class PendingTransaction(Base):
     signature = Column(String, nullable=False)
 
     time = Column(DateTime, nullable=False, unique=False, default=datetime.utcnow)
+
+
+class Transaction(Base):
+    __tablename__ = "Transactions"
+
+    height = Column(Integer, nullable=False, unique=True)
+    uuid = Column(String, primary_key=True, nullable=False, unique=True)
+
+    sender = Column(String, nullable=False)
+    target = Column(String, nullable=False)
+    value = Column(Float, nullable=False)
+    signature = Column(String, nullable=False)
+
+    time = Column(DateTime, nullable=False, unique=False)
+
+    miner = Column(String, nullable=False, unique=False)
+    nonce = Column(Integer, nullable=False, unique=False)
+    prev_hash = Column(String, nullable=False, unique=True)
+
 
 def get_db():
     try:
