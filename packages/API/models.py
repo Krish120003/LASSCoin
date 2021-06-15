e5d02dd358fb2197eee3c493cd99f124197d6c55
Krish120003 <krish120003@gmail.com>
2021-06-15 18:21:38 -0400

    Add validator to prevent duplicate transactions

--- packages/API/models.py
+++ packages/API/models.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, validator
 from KeyManager import verify
 
+from db import get_db, PendingTransaction
 
 class CreateTransactionContext(BaseModel):
     sender: str
@@ -8,6 +9,14 @@ class CreateTransactionContext(BaseModel):
     value: float
     uuid: str
     signature: str
+    
+    @validator("uuid")
+    def verify_uniqueness(cls, v, values):
+        db = next(get_db())
+        res = db.query(PendingTransaction).filter_by(uuid=v).all()
+        if res:
+            raise ValueError("Transaction Exists")
+        return v
 
     @validator("signature")
     def verify_legitamicy(cls, v, values):
