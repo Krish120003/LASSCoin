f3fca74176c837a7438345a22ed243326fc5c997
AidanP771 <aidanm771@outlook.com>
2021-06-14 21:54:31 -0400

    Made PublicTransactionsList
    Its work for what is needs to I just am not sure how to separate each element in the array to a different line

--- packages/web/components/PublicTransactionList.js
+++ packages/web/components/PublicTransactionList.js
@@ -1,12 +1,17 @@
 import {useState} from "react";
 
-export default function PublicTransactionList() {
+
+export default function PublicTransactionList(props) {
     const [PublicTransactions, setPublicTransactions] = useState(0);
+    
+    //example cause no fluid data
+    const extransactions = ["1111","2222","3333","4444"]
+    const extransactionslist = extransactions.map ((extransactions) => extransactions * 2);
 
     return (
         <div>
             <h4> Example Transaction </h4>
-            <PublicTransactions/>
+            {extransactions}
         </div>
     )
 }
--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import MyApp from './_app'
 import BalanceCard from '../components/BalanceCard'
+import PublicTransactionList from '../components/PublicTransactionList'
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <Head> 
           <title> PAIN </title>
         </Head>
+        <PublicTransactionList/>
       </>
   )
 };
