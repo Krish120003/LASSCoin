576c30f9ee93b33407d086a37098905f970e5548
Krish120003 <krish120003@gmail.com>
2021-06-15 20:48:04 -0400

    Update main.py
    Update click params, add imports, setup main loop

--- packages/miner/main.py
+++ packages/miner/main.py
@@ -1,17 +1,38 @@
 import click
 from urllib.parse import urljoin
 import requests
+import sys
+import time
+from hashlib import sha256
 
 
 @click.command()
-@click.option("--server", default="http://127.0.0.1", help="Server URL")
+@click.option("--server", default="http://127.0.0.1:8000", help="Server URL")
 @click.option(
     "--max-nonce", default=2 ** 32 - 1, help="The maximum nonce to try when mining."
 )
-def main(server, max_nonce):
+@click.option("--address", help="Miner's public address.", required=True)
+def main(server, max_nonce, address):
     click.echo("Starting LASSCoin Miner. Use --help to see all CLI options.")
 
     req_url = urljoin(server, "/api/miner/")
+
+    while True:
+        try:
+            # Get block data to mine
+            response = requests.get(req_url)
+            block = response.json()
+
+            click.echo(block)
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            click.echo("\rExiting...")
+            sys.exit(0)
+        except Exception as e:
+            click.echo("An exception has occured. Trying again in 1 min.")
+            click.echo(e)
+            time.sleep(60)
 
 
 if __name__ == "__main__":
