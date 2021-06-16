fb1ab8ed3690d78eda6b7e198872082d0998584c
Krish120003 <krish120003@gmail.com>
2021-06-16 17:30:05 -0400

    Setup model for transactions verified by miners

--- packages/API/models.py
+++ packages/API/models.py
@@ -1,7 +1,9 @@
+from util import check_hash, build_binary_block_data
 from pydantic import BaseModel, validator
 from KeyManager import verify
 
 from db import get_db, PendingTransaction
+
 
 class CreateTransactionContext(BaseModel):
     sender: str
@@ -9,7 +11,7 @@ class CreateTransactionContext(BaseModel):
     value: float
     uuid: str
     signature: str
-    
+
     @validator("uuid")
     def verify_uniqueness(cls, v, values):
         db = next(get_db())
@@ -23,4 +25,27 @@ class CreateTransactionContext(BaseModel):
         msg = f'{values.get("uuid")}|{values.get("sender")}|{values.get("target")}|{values.get("value"):.15f}'
         if not verify(values.get("sender"), msg, v):
             raise ValueError("Invalid Signature")
+        return v
+
+
+class MinedTransactionData(BaseModel):
+    uuid: str
+    height: int
+    time: int
+
+    sender: str
+    target: str
+    value: float
+    signature: str
+
+    prev_hash: str
+    miner: str
+
+    nonce: int
+
+    @validator("nonce")
+    def verify_nonce(cls, v, values):
+        values["nonce"] = v
+        if not check_hash(build_binary_block_data(values)):
+            raise ValueError("Invalid block hash.")
         return v
