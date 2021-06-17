4bfd5babdd86902079b2c6f5e149e32e584b691c
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 10:39:05 -0400

    Create StatStack.js

---
+++ packages/web/components/StatStack.js
@@ -0,0 +1,13 @@
+import StatCard from "./StatCard"
+
+export default function StatStack() {
+    return (
+        <div>
+            <h2>BLOCKCHAIN STATS</h2>
+            <div>
+                <StatCard/>
+                <StatCard/>
+            </div>
+        </div>
+    )
+}
