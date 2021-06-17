0c46ac968f935019256a52458371db7ae7da7554
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 09:48:12 -0400

    Structure content

--- packages/web/components/BalanceCard.js
+++ packages/web/components/BalanceCard.js
@@ -6,7 +6,13 @@ export default function BalanceCard() {
   return (
     <div className={styles.balance_card}>
       <h3 className={styles.thold_title}>Balance Details</h3>
-      <TextColumnGroup />
+      <div>
+        <TextColumnGroup title="Current Balance" value={2.2500200}/>
+        <div>
+          <TextColumnGroup title="Lifetime Mined" value={3.2500000}/>
+          <TextColumnGroup title="Lifetime Received" value={0.0000200}/>
+        </div>
+      </div>
       <div className={styles.buttons}>
         <BalanceCardButton value={"Receive"} red={false} />
         <BalanceCardButton value={"Send"} red={true} />
