f0bfbfccc7da82e44a60c710f26e99f3916d14fb
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 11:16:55 -0400

    Update main page

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
-import BalanceCard from "../components/BalanceCard";
 
 import styles from "../styles/index.module.scss";
+
+import BalanceCard from "../components/BalanceCard";
+import StatStack from "../components/StatStack";
 
 export default function Home() {
   return (
     <div className={styles.layout}>
       <BalanceCard />
+      <StatStack />
     </div>
   );
 }
-
