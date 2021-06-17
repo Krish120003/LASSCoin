5ef1bf2704fa83fbb781b3739c0051a25d088375
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 10:04:22 -0400

    setup class passthrough

--- packages/web/components/TextColumnGroup.js
+++ packages/web/components/TextColumnGroup.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function TextColumnGroup(props) {
   return (
-    <div>
+    <div className={props.class}>
       <h4>L$ {props.value ? props.value.toFixed(6) : ""}</h4>
       <p>{props.title}</p>
     </div>
