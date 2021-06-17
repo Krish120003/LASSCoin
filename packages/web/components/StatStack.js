d1d0abe026e895fb0aa2b0ffc40662996934f985
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 10:40:00 -0400

    Autoformat

--- packages/web/components/StatStack.js
+++ packages/web/components/StatStack.js
@@ -1,13 +1,13 @@
-import StatCard from "./StatCard"
+import StatCard from "./StatCard";
 
 export default function StatStack() {
-    return (
-        <div>
-            <h2>BLOCKCHAIN STATS</h2>
-            <div>
-                <StatCard/>
-                <StatCard/>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h2>BLOCKCHAIN STATS</h2>
+      <div>
+        <StatCard title={"Height"} value={1337} />
+        <StatCard title={"Height"} value={1337} />
+      </div>
+    </div>
+  );
 }
