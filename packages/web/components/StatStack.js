275cc1c001d21eae8f643554b925bf1900c0b1ef
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 10:57:30 -0400

    setup styles

--- packages/web/components/StatStack.js
+++ packages/web/components/StatStack.js
@@ -1,9 +1,11 @@
 import StatCard from "./StatCard";
 
+import styles from "../styles/StatStack.module.scss";
+
 export default function StatStack() {
   return (
-    <div>
-      <h2>BLOCKCHAIN STATS</h2>
+    <div className={styles.stat_stack}>
+      <h2 classNa>BLOCKCHAIN STATS</h2>
       <div>
         <StatCard title={"Height"} value={1337} />
         <StatCard title={"Height"} value={1337} />
