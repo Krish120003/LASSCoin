2cdc6f28508b25cbf8ec94f4ea3f5fb687509282
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 10:05:07 -0400

    Update styling

--- packages/web/components/BalanceCard.js
+++ packages/web/components/BalanceCard.js
@@ -7,10 +7,10 @@ export default function BalanceCard() {
     <div className={styles.balance_card}>
       <h3 className={styles.thold_title}>Balance Details</h3>
       <div>
-        <TextColumnGroup title="Current Balance" value={2.2500200}/>
-        <div>
-          <TextColumnGroup title="Lifetime Mined" value={3.2500000}/>
-          <TextColumnGroup title="Lifetime Received" value={0.0000200}/>
+        <TextColumnGroup class={styles.main_text_column} title="Current Balance" value={2.2500200}/>
+        <div className={styles.flex_split}>
+          <TextColumnGroup class={styles.mini_text_column} title="Lifetime Mined" value={3.2500000}/>
+          <TextColumnGroup class={styles.mini_text_column} title="Lifetime Received" value={0.0000200}/>
         </div>
       </div>
       <div className={styles.buttons}>
