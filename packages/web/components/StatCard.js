f8c719227749682d1d2a7af2496c49860b1e8d0a
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-17 10:34:24 -0400

    Setup props

--- packages/web/components/StatCard.js
+++ packages/web/components/StatCard.js
@@ -3,8 +3,8 @@ import styles from "../styles/StatCard.module.scss"
 export default function StatCard(props) {
     return (
         <div className={styles.stat_card}>
-            <h4>Stat</h4>
-            <h6>Title</h6>
+            <h4>{props.value}</h4>
+            <h6>{props.title}</h6>
         </div>
     )
 }
