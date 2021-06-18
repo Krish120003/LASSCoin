8d06d81dfecfc446792b863af240a9dd9c930c01
AidanP771 <aidanm771@outlook.com>
2021-06-18 02:12:07 -0400

    Updated PublicTransaction list to work with data

--- packages/web/components/PublicTransactionList.js
+++ packages/web/components/PublicTransactionList.js
@@ -1,20 +1,23 @@
 import {useState} from "react";
 import styles from "../styles/Transactions.module.scss";
+import data from "./TransactionData";
+
+
+
 
 export default function PublicTransactionList(props) {
     const [PublicTransactions, setPublicTransactions] = useState(0);
     
     //example cause no fluid data
-    const extransactions = ["1111","2222","3333","4444"]
-    const extransactionslist = extransactions.map ((extransactions) => extransactions * 2);
-
+    // const extransactions = ["1111","2222","3333","4444"]
+    console.log(data);
     return (
         <div>
             <h4> User Transaction </h4>
             <ul className={styles.styled_list}>
               {
-                  extransactions.map(x => {
-                      return <li>{x}</li>
+                  data.map(x => {
+                      return <li key={x.uuid}>{x.height} {x.time} {x.value}</li>
                   })
               }  
             </ul>
