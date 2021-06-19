9eff6233368fc0bef217a09449d45768faedc356
Krish120003 <krish120003@gmail.com>
2021-06-19 19:01:42 -0400

    Setup icon inside button comp

--- packages/web/components/SidebarButton.js
+++ packages/web/components/SidebarButton.js
@@ -1,5 +1,15 @@
 import styles from "../styles/SidebarButton.module.scss";
 
 export default function SidebarButton(props) {
-  return <div className={styles.main}>{props.children}</div>;
+  return (
+    <div className={styles.main}>
+      {props.children}
+      <span
+        className="iconify"
+        data-icon={props.iconName}
+        data-inline="false"
+        data-width="45%"
+      ></span>
+    </div>
+  );
 }
