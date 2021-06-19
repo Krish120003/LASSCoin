f7604791d7ca4fcaf5fb9c54514e66a11048fa8c
AidanP771 <aidanm771@outlook.com>
2021-06-18 21:23:25 -0400

    Attempt #1 at being able to upload a file
    I have no idea why but everything compiled fine, but the button just doesn't wanna show up

--- packages/web/pages/loginpage.js
+++ packages/web/pages/loginpage.js
@@ -4,11 +4,13 @@ import styles from "../styles/index.module.scss";
 
 import BalanceCard from "../components/BalanceCard";
 import StatStack from "../components/StatStack";
+import fileupload from "../components/fileupload";
 
 export default function Home() {
   return (
     <div className={styles.layout}>
       <h1> Please Import Your Private Key File</h1>
+      <fileupload/>
     </div>
   );
 }
