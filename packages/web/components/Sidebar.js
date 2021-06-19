4bdef736ff5723bff6d994a40fbf4922e57a0cd9
Krish120003 <krish120003@gmail.com>
2021-06-19 19:01:48 -0400

    Setup button icons

--- packages/web/components/Sidebar.js
+++ packages/web/components/Sidebar.js
@@ -3,6 +3,7 @@ import styles from "../styles/Sidebar.module.scss";
 import SidebarButton from "./SidebarButton";
 
 export default function Sidebar() {
+  const currentPage = 0;
   return (
     <div className={styles.main}>
       <div className={styles.logo}>
@@ -14,15 +15,12 @@ export default function Sidebar() {
           style={{ color: "#5C4E7A" }}
         ></span>
       </div>
-      <SidebarButton>
-        <span
-          className="iconify"
-          data-icon="akar-icons:home"
-          data-inline="false"
-        ></span>
-      </SidebarButton>
-      <SidebarButton />
-      <SidebarButton />
+      <SidebarButton iconName="akar-icons:home" active={currentPage == 0} />
+      <SidebarButton
+        iconName="radix-icons:activity-log"
+        active={currentPage == 1}
+      />
+      <SidebarButton iconName="bx:bx-log-out" />
     </div>
   );
 }
