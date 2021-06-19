30e68fe4e89215ba7ab2fe6c6e54965808d840bd
Krish120003 <krish120003@gmail.com>
2021-06-19 15:00:47 -0400

    Add icon

--- packages/web/components/Sidebar.js
+++ packages/web/components/Sidebar.js
@@ -8,6 +8,12 @@ import SidebarButton from "./SidebarButton";
 export default function Sidebar() {
   return (
     <div className={styles.main}>
+      <Icon
+        icon={coinIcon}
+        width="3em"
+        color="#5C4E7A"
+        style={{ "margin-bottom": "4em", "margin-left": "0.5em" }}
+      />
       <SidebarButton />
       <SidebarButton />
       <SidebarButton />
