843f54f9432b40d5af9cf6830025cd5b29dee96d
AidanP771 <aidanm771@outlook.com>
2021-06-20 00:45:00 -0400

    Create PrivateKeyProcessor.js

---
+++ packages/web/components/PrivateKeyProcessor.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+import {key} from "./FileUploader";
+
+
+export default function PrivateKeyProcessor() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
