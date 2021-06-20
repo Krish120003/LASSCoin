1ad130e9dab0ba92719e1c4dbb1bb5311c4b46d0
AidanP771 <aidanm771@outlook.com>
2021-06-20 02:13:22 -0400

    Removed file processor render and import

--- packages/web/pages/loginpage.js
+++ packages/web/pages/loginpage.js
@@ -5,15 +5,12 @@ import styles from "../styles/index.module.scss";
 import BalanceCard from "../components/BalanceCard";
 import StatStack from "../components/StatStack";
 import FileUploader from "../components/FileUploader";
-import { FileProcessor } from "../components/FileUploader";
-
 
 export default function Home() {
   return (
     <div className={styles.layout}>
       <h1> Private Key Login</h1>
       <FileUploader/>
-      <FileProcessor/>
     </div>
   );
 }
