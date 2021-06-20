8a3f936f0827c73a7a993b75fef5ca5f735d3781
Krish120003 <krish120003@gmail.com>
2021-06-20 14:08:15 -0400

    Add timestamp formatting

--- packages/web/components/TransactionLI.js
+++ packages/web/components/TransactionLI.js
@@ -1,11 +1,13 @@
 import styles from "../styles/TransactionLI.module.scss";
 
+import { formatDate } from "../util/parseTime";
+
 export default function TransactionLI(props) {
   return (
     <li className={styles.main}>
       <span className={styles.left}>
         <h6>Height {props.height}</h6>
-        <p>{props.timestamp}</p>
+        <p>{formatDate(props.timestamp * 1000, "d MMM yyyy H:mm:ss")}</p>
       </span>
       <span className={styles.right}>
         {props.value ? props.value.toFixed(14) : ""}
