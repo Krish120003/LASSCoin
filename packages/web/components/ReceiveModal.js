af6233c086d669450733d09ea39f525bf29ae83d
Krish120003 <krish120003@gmail.com>
2021-06-21 17:56:01 -0400

    Create base modal comp

---
+++ packages/web/components/ReceiveModal.js
@@ -0,0 +1,11 @@
+import styles from "../styles/ReceiveModal.module.scss"
+
+export default function ReceiveModal() {
+    return (
+        <div className={styles.main}>
+            <div className={styles.content}>
+                x
+            </div>
+        </div>
+    )
+}
