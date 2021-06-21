e8122bee7c4bbccc22be40f2743fee8c86f2d1cb
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-21 10:50:36 -0400

    setup basics reducer action

--- packages/web/store.js
+++ packages/web/store.js
@@ -8,7 +8,29 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "GET_TRANSACTIONS":
-      return { ...state };
+      const newTransactions = [...state.transactions];
+      let newNext = null;
+      if (state.nextTransaction != null) {
+      } else {
+        fetch("https://lasscoin.herokuapp.com/api/transactions/").then(
+          (res) => {
+            res.json().then((data) => {
+              newNext = data.next ? data.next : null;
+              data.data.forEach((element) => {
+                newTransactions.push(element);
+              });
+            });
+          }
+        );
+        newTransactions;
+      }
+      return {
+        ...state,
+        transactions: newTransactions,
+        nextTransaction: newNext,
+      };
+    default:
+      return state;
   }
 };
 
