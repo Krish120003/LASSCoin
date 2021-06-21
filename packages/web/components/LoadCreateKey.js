282ec507e4b6a4b515dbc29cfc80a2e44439e5b1
Krish120003 <krish120003@gmail.com>
2021-06-20 21:14:22 -0400

    Setup button colors and title

--- packages/web/components/LoadCreateKey.js
+++ packages/web/components/LoadCreateKey.js
@@ -3,8 +3,14 @@ import styles from "../styles/LoadCreateKey.module.scss";
 export default function LoadCreateKey() {
   return (
     <div className={styles.main}>
-      <div className={styles.title_div}>WELCOME TO LASSCoin</div>
-      <div className={styles.content}></div>
+      <div className={styles.title_div}>
+        <div>WELCOME TO LASSCoin</div>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.load_btn}></div>
+        <div className={styles.create_btn}></div>
+      </div>
     </div>
   );
 }
