e134587035c41ff1ce1a07e4b51e8ae93275fb8f
Krish120003 <krish120003@gmail.com>
2021-06-21 17:56:44 -0400

    Setup button actions, states, basic modal

--- packages/web/components/BalanceCard.js
+++ packages/web/components/BalanceCard.js
@@ -1,34 +1,54 @@
+import {useState} from "react";
+import Modal from "react-modal";
+
 import styles from "../styles/Balance.module.scss";
+
 import TextColumnGroup from "./TextColumnGroup";
 import BalanceCardButton from "./BalanceCardButton";
+import ReceiveModal from "./ReceiveModal";
 
 export default function BalanceCard() {
+
+  const [receiveModalOpen, setReceiveModalOpen] = useState(false);
+  const [sendModalOpen, setSendModalOpen] = useState(false);
+
   return (
-    <div className={`${styles.balance_card} flex_margin`}>
-      <h3 className={styles.thold_title}>Balance Details</h3>
-      <div>
-        <TextColumnGroup
-          class={styles.main_text_column}
-          title="Current Balance"
-          value={2.25002}
-        />
-        <div className={styles.flex_split}>
+    <>
+      <Modal
+        isOpen={receiveModalOpen}
+        ariaHideApp={false} 
+        className={styles.modal_style} 
+        shouldCloseOnOverlayClick={true} 
+        onRequestClose={() => setReceiveModalOpen(false)}
+      >
+        <ReceiveModal />
+      </Modal>
+      <div className={`${styles.balance_card} flex_margin`}>
+        <h3 className={styles.thold_title}>Balance Details</h3>
+        <div>
           <TextColumnGroup
-            class={styles.mini_text_column}
-            title="Lifetime Mined"
-            value={3.25}
+            class={styles.main_text_column}
+            title="Current Balance"
+            value={2.25002}
           />
-          <TextColumnGroup
-            class={styles.mini_text_column}
-            title="Lifetime Received"
-            value={0.00002}
-          />
+          <div className={styles.flex_split}>
+            <TextColumnGroup
+              class={styles.mini_text_column}
+              title="Lifetime Mined"
+              value={3.25}
+            />
+            <TextColumnGroup
+              class={styles.mini_text_column}
+              title="Lifetime Received"
+              value={0.00002}
+            />
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          <BalanceCardButton value={"Receive"} red={false} onClick={() => {setReceiveModalOpen(true)}}/>
+          <BalanceCardButton value={"Send"} red={true} />
         </div>
       </div>
-      <div className={styles.buttons}>
-        <BalanceCardButton value={"Receive"} red={false} />
-        <BalanceCardButton value={"Send"} red={true} />
-      </div>
-    </div>
+    </>
   );
 }
