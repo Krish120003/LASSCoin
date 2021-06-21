1aae788a00dfe427690ae3bb101f6c543665d352
Krish120003 <krish120003@gmail.com>
2021-06-20 20:54:00 -0400

    Setup LoadCreateKey comp

---
+++ packages/web/components/LoadCreateKey.js
@@ -0,0 +1,10 @@
+import styles from "../styles/LoadCreateKey.module.scss";
+
+export default function LoadCreateKey() {
+  return (
+    <div className={styles.main}>
+      <div className={styles.title_div}>WELCOME TO LASSCoin</div>
+      <div className={styles.content}></div>
+    </div>
+  );
+}
