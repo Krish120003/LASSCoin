8016841f1159f30def9ce52d793fb4c0547e9cd2
Krish120003 <krish120003@gmail.com>
2021-06-21 17:56:32 -0400

    Add onClick passthrough prop

--- packages/web/components/BalanceCardButton.js
+++ packages/web/components/BalanceCardButton.js
@@ -4,6 +4,7 @@ export default function BalanceCardButton(props) {
   return (
     <div
       className={!props.red ? styles.button : `${styles.button} ${styles.red}`}
+      onClick={props.onClick}
     >
       <p className={styles.button_text}>{props.value}</p>
     </div>
