946ef08da43f901cb411972e9413742d5133f911
AidanP771 <aidanm771@outlook.com>
2021-06-21 23:43:50 -0400

    Create Crypto.js

---
+++ reference/Crypto.js
@@ -0,0 +1,38 @@
+function generateUUID4String () {
+    // return uuid
+}
+
+function importPrivateKey(privateKeyText) {
+    // return CryptoKey object
+}
+
+function getPublicKey(privateKey) {
+    // return CryptoKey object
+}
+
+function getPublicKeyAsText(publicKey) {
+    // return string
+}
+
+function sign(sender, target, uuid, amount, signingPrivateKey) {
+    // First, create string "message"
+    // The message format is in DMs from a few days ago
+    // Then get hash of message
+    // And then sign it using the signingPrivateKey
+
+    /*
+    Sample Data (valid):
+    (Use these values to test your stuff)
+
+    {
+        "sender":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxhrRjWzQUKHLOve5TPOd3dE0WUg1gSws9bdT1U2yBxBRGZevxSIe1R25E9TcbMCim8pj1fPjmJh0Z3RsDuOxQxFQe/y2XV38ezTV86XuwgCj/Q6jhPVn0pKzn9IWvSKzZ4E+vwCfcpG9Z3fyBcm2qJ0nICjHPZp9MbbJmBqK04gnl2374ZnxgqIzhRDmdZy73ZWW24fXHP2XZ+aYa2GS8qEu5eMQ7BuP35dA+kOrX6R2NZRhXfV0OZV58HmKt7m3tj6gQhtr8TjNA/YcE3zQAG6yEubsm/l7Zlscnvz+3M9Zsvz2wJIRE13RmYScDJ+9pS0kncSp2Tof6BiI8p0UQwIDAQAB",
+        "target":"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArUJ3miD2D2mR/83TeqfV9gt/9Q47FCJYmQltdNh+3JpKbjP7rxArgwGB/5mzHG6/SwtJSg5GYnCuI8pYBtq2LiDPPadDRnLb2dAulcUW2bkdK6xo6LeiCwfNxeEWlZW0kKRcqt1+0A4MyvO0AVanyQGVmt1XOGqDaID1dPO861gHpgrQZes+5zH9UOLGrVKrytQPp4wzn9azW74ErDcz2obMXvDmMMXnrBDCcUErbDCPPXBTKaLlAd+wdquvEOJcSdZfa74O/YaFQx0Gw4N3t8UwS8VSf9Wo5miWH4p2CGACIi4foM/vfhGnlWQ41gImTYN6WVZA4j3ZR2eyKMUMhwIDAQAB",
+        "value":3.984580988793731,
+        "signature":"Y5M1UaNUYh1yd7sgdZuoxf9PNpQ6qoV3DDqCnbTEe6dX4rb1DZOt3oIzqFQvRJ/5Wm2Qse/3XVss74/Jy28W1npPTpptkMMxczzn2VZXiU/6eqlDia/PYw9nGfCdKezhI8NxIT7eJfJLryf033euLjGx83TjIFe9lTzHObKFsyybZ/DApx8MzRyfHn1I9X3FC53R2cftN1Qgp8uRX1Aj/HaHTtRxLQMHyny/rTyOyd2t0cVutp1llQU4Y3F8dDx3e7bGRDFR84Hm1pyxYZvmtpt3ffM7acjHaFXTDuOqrif4U0I/aIBzaO80EMIlUIearvCynKDoQMjJJPgC4LYUiw==",
+        "uuid":"3d14dffa-6eee-4c3f-b539-1e10dded4de5"
+    }
+
+    */
+
+    // return string
+}
