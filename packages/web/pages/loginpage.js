da354bd3bd4cac95d3ca5c3d7e24e86e6d5fca9f
AidanP771 <aidanm771@outlook.com>
2021-06-22 00:20:51 -0400

    Update loginpage.js

--- packages/web/pages/loginpage.js
+++ packages/web/pages/loginpage.js
@@ -5,13 +5,14 @@ import styles from "../styles/index.module.scss";
 import BalanceCard from "../components/BalanceCard";
 import StatStack from "../components/StatStack";
 import FileUploader from "../components/FileUploader";
+import Crypto from "../components/Crypto";
 
 
 export default function Home() {
   return (
     <div className={styles.layout}>
       <h1> Private Key Login</h1>
-      <FileUploader/>
+      <Crypto/>
     </div>
   );
 }
