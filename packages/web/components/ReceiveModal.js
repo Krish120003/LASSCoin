d4cbe93e3b613bdff7c142e631040dd99e6c14c9
Krish120003 <krish120003@gmail.com>
2021-06-21 21:13:10 -0400

    Add copy functionality

--- packages/web/components/ReceiveModal.js
+++ packages/web/components/ReceiveModal.js
@@ -7,10 +7,37 @@ export default function ReceiveModal() {
         <div className={styles.center}>
           <h3>Receive LASSCoin</h3>
           <div className={styles.address_box}>
-            <div className={styles.key_box}>
+            <div id="public_key_copy" className={styles.key_box}>
               MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxhrRjWzQUKHLOve5TPOd3dE0WUg1gSws9bdT1U2yBxBRGZevxSIe1R25E9TcbMCim8pj1fPjmJh0Z3RsDuOxQxFQe
             </div>
-            <div className={styles.copy_button}>Copy</div>
+            <div
+              className={styles.copy_button}
+              onClick={() => {
+                function selectText(node) {
+                  node = document.getElementById(node);
+
+                  if (document.body.createTextRange) {
+                    const range = document.body.createTextRange();
+                    range.moveToElementText(node);
+                    range.select();
+                  } else if (window.getSelection) {
+                    const selection = window.getSelection();
+                    const range = document.createRange();
+                    range.selectNodeContents(node);
+                    selection.removeAllRanges();
+                    selection.addRange(range);
+                  } else {
+                    console.warn(
+                      "Could not select text in node: Unsupported browser."
+                    );
+                  }
+                }
+                selectText("public_key_copy");
+                document.execCommand("copy");
+              }}
+            >
+              Copy
+            </div>
           </div>
           <p>
             This is your public address that can be used by others to send you
