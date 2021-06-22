f92e664882bffd79d16d38cca01a0337d57a44b3
Krish <52761222+Krish120003@users.noreply.github.com>
2021-06-22 10:59:21 -0400

    Add inputs, setup local states

--- packages/web/components/SendModal.js
+++ packages/web/components/SendModal.js
@@ -4,10 +4,22 @@ import styles from "../styles/SendModal.module.scss";
 import React from "react";
 
 export default function SendModal() {
+  const [target, setTarget] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <div className={styles.main}>
       <div className={styles.content}>
-        <input type="number"></input>
+        <input
+          type="text"
+          onChange={(e) => {
+            setTarget(e.target.value);
+          }}
+        ></input>
+        <input
+          type="number"
+          onChange={(e) => setAmount(e.target.value)}
+        ></input>
       </div>
     </div>
   );
