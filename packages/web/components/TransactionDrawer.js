72d2831d69ea8eb77cac9c8d3bf6df1f2ecf52ab
Krish120003 <krish120003@gmail.com>
2021-06-22 21:42:42 -0400

    remove extra log

--- packages/web/components/TransactionDrawer.js
+++ packages/web/components/TransactionDrawer.js
@@ -6,7 +6,6 @@ import styles from "../styles/TransactionDrawer.module.scss";
 import TransactionLI from "./TransactionLI";
 
 function TransactionDrawer(props) {
-  console.log(props);
   const [filter, setFilter] = useState(false);
   return (
     <div className={props.full ? styles.full : styles.main}>
