29c8f591ec99761c9b18a9861d24569f5db7cee8
Krish120003 <krish120003@gmail.com>
2021-06-22 21:29:55 -0400

    Setup index for transactions

--- packages/web/pages/transactions/index.js
+++ packages/web/pages/transactions/index.js
@@ -1,5 +1,20 @@
-import React from "react";
+import { useDispatch, connect } from "react-redux";
+
+import styles from "../../styles/index.module.scss";
+
+import Sidebar from "../../components/Sidebar";
+import TransactionDrawer from "../../components/TransactionDrawer";
 
 export default function index() {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  dispatch({ type: "GET_TRANSACTIONS" });
+  setInterval(() => dispatch({ type: "GET_TRANSACTIONS" }), 10000);
+
+  return (
+    <div className={styles.layout_t}>
+      <Sidebar />
+      <TransactionDrawer full={true}></TransactionDrawer>
+    </div>
+  );
 }
