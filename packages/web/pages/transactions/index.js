9f2288b51b39eb1942aa2162bb29b04aa083f8ca
Krish120003 <krish120003@gmail.com>
2021-06-22 22:20:47 -0400

    Setup hook to unload interval

--- packages/web/pages/transactions/index.js
+++ packages/web/pages/transactions/index.js
@@ -1,5 +1,5 @@
 import { useDispatch, connect } from "react-redux";
-
+import { useEffect } from "react";
 import styles from "../../styles/index.module.scss";
 
 import Sidebar from "../../components/Sidebar";
@@ -9,7 +9,20 @@ export default function index() {
   const dispatch = useDispatch();
 
   dispatch({ type: "GET_TRANSACTIONS" });
-  setInterval(() => dispatch({ type: "GET_TRANSACTIONS" }), 10000);
+  dispatch({ type: "GET_HEIGHT" });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      dispatch({ type: "GET_TRANSACTIONS" });
+    }, 1000);
+    const secondIntervalId = setInterval(() => {
+      dispatch({ type: "GET_HEIGHT" });
+    }, 10000);
+    return () => {
+      clearInterval(intervalId);
+      clearInterval(secondIntervalId);
+    };
+  }, []);
 
   return (
     <div className={styles.layout_t}>
